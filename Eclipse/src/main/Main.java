a360a1dd7e3a5b5f149462c2b9a80354b67b292d
cristian <crisdamencast@gmail.com>
2021-04-18 00:35:47 -0500

    eclipse

---
+++ Eclipse/src/main/Main.java
@@ -0,0 +1,15 @@
+package main;
+
+import processing.core.PApplet;
+
+public class Main extends PApplet {
+
+	public static void main(String[] args) {
+		PApplet.main("main.Main");
+	}
+	
+	public void settings() {
+		size(1200, 700);
+	}
+
+}
